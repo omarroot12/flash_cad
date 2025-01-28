037a608eacfb1451dd945c109014adb04e4b3a58
omarroot12 <48679506+omarroot12@users.noreply.github.com>
2025-01-28 17:35:53 +0300

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    // تفعيل إضافة المستخدمين والأذونات (users-permissions)
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET'), // تأكد من أنك قد قمت بتعيين JWT_SECRET في متغيرات البيئة
+      },
+    },
+  
+    // تفعيل إضافة الوثائق (documentation)
+    documentation: {
+      enabled: true,
+      config: {
+        // يمكنك تخصيص إعدادات الوثائق هنا إذا لزم الأمر
+      },
+    },
+  });
